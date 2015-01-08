f2c5a04393827ed8664fe80d68a1636f77c64cd2
Ebony <ebonynyenya@Ebonys-MacBook-Air.local>
2015-01-08 09:25:09 -0500

    Start GithubFriends

---
+++ GithubFriends/GithubFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubFriends
+//
+//  Created by Ebony Nyenya on 1/8/15.
+//  Copyright (c) 2015 Ebony Nyenya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
