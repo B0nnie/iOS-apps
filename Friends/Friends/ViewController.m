eac70e8ac99c43e167e961f2ae9c57e9ee0b457f
Ebony <esn05@yahoo.com>
2015-01-22 21:09:13 -0500

    Friends and FunFacts
    A basic messaging app and an app that displays a different fact each
    time a button is pressed.

---
+++ Friends/Friends/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Friends
+//
+//  Created by Ebony Nyenya on 1/22/15.
+//  Copyright (c) 2015 Ebony Nyenya. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Parse/Parse.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    testObject[@"name"] = @"Ebony";
+    [testObject saveInBackground];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FunFacts/FunFacts/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Ebony Nyenya on 1/22/15.
+//  Copyright (c) 2015 Ebony Nyenya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *funFactLabel;
+
+@end
+
